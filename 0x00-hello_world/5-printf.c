06e7e2ead13fc7a57917ac2812711cd58db89c46
cherryudezze <cherryudezze@gmail.com>
2023-02-17 16:40:33 -0800

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * Return: Always returns (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
