c01c2b710ab8d2cca1a8181bb52be6a8a5b73453
cherryudezze <cherryudezze@gmail.com>
2023-02-22 05:58:18 -0800

    alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+        int i, co;
+
+	co = 0;
+
+	while (co < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		co++;
+		_putchar('\n');
+	}
+}
+~ 
