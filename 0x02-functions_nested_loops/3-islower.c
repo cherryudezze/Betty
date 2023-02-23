a50a89e116c95f402f41620e62523447b4846222
cherryudezze <cherryudezze@gmail.com>
2023-02-22 16:29:16 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/** 
- * _islower - Entry point 
- * Description: checks is a character is lowercase 
- * @c: the integer value it receives 
+/**
+ * _islower - Entry point
+ * Description: checks is a character is lowercase
+ * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
 int _islower(int c)
