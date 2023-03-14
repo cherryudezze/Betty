3ca517018c666944f3cb7ea81ad6f0ef99f54a7c
cherryudezze <cherryudezze@gmail.com>
2023-03-13 18:29:43 -0700

    Create the static library libmy.a containing all the functions listed below

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ * Auth: Cherry
+ * Desc: header files contain all the prototypes of the below functions.
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +27,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
